73a6f9f7c514c61178507ff8c1ca7aee78f04552
Joshua Carew <joshua.carew@yahoo.com>
2020-06-02 16:52:49 +0100

    Updates

---
+++ precache-manifest.cc35af9db03f95036063c4c869a9d0d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9d30107b61a6efb8fcb78012edb121c",
+    "url": "/library-app-react/index.html"
+  },
+  {
+    "revision": "bcf8c6d1cb6827c1dff9",
+    "url": "/library-app-react/static/css/main.226ebbf0.chunk.css"
+  },
+  {
+    "revision": "ca96040d0f0ae74eddb8",
+    "url": "/library-app-react/static/js/2.1dee5b28.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/library-app-react/static/js/2.1dee5b28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcf8c6d1cb6827c1dff9",
+    "url": "/library-app-react/static/js/main.497f80eb.chunk.js"
+  },
+  {
+    "revision": "10fa39414f07bf76703e",
+    "url": "/library-app-react/static/js/runtime-main.3d7492c5.js"
+  }
+]);
